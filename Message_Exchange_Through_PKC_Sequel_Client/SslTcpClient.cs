0d46cf5c632595e95e9914c4f5081b2d68e37a77
Ruben van Oers <r.vanoers1@student.avans.nl>
2020-06-04 20:04:02 +0200

    added hash verification on the server side

--- Message_Exchange_Through_PKC_Sequel_Client/SslTcpClient.cs
+++ Message_Exchange_Through_PKC_Sequel_Client/SslTcpClient.cs
@@ -10,129 +10,125 @@ using System.Text;
 
 namespace Message_Exchange_Through_PKC_Sequel_Client
 {
-    public class SslTcpClient
-    {
-        private static Hashtable certificateErrors = new Hashtable();
-        public static int Main(string[] args)
-        {
-	        string serverCertificateName = "HP-Envy-Ruben";
-	        string machineName = "HP-Envy-Ruben";
+	public class SslTcpClient
+	{
+		private static Hashtable certificateErrors = new Hashtable();
+		private static X509Certificate2 localCertificate2 = null;
 
-	        SslTcpClient.RunClient(machineName, serverCertificateName);
-	        return 0;
-        }
+		public static int Main(string[] args)
+		{
+			string serverCertificateName = "HP-Envy-Ruben";
+			string machineName = "HP-Envy-Ruben";
 
-        public static bool ValidateServerCertificate(
-              object sender,
-              X509Certificate certificate,
-              X509Chain chain,
-              SslPolicyErrors sslPolicyErrors)
-        {
-            if (sslPolicyErrors == SslPolicyErrors.None)
-                return true;
+			string certificate = Directory.GetCurrentDirectory() + "\\client.pfx";
+			localCertificate2 = new X509Certificate2(certificate, "secret");
 
-            Console.WriteLine("Certificate error: {0}", sslPolicyErrors);
+			SslTcpClient.RunClient(machineName, serverCertificateName);
+			return 0;
+		}
 
-          
-            return false;
-        }
-        public static void RunClient(string machineName, string serverName)
-        {
-         
-            TcpClient client = new TcpClient(machineName, 8080);
-            Console.WriteLine("Client connected.");
-   
-            SslStream sslStream = new SslStream(
-                client.GetStream(),
-                false,
-                new RemoteCertificateValidationCallback(ValidateServerCertificate),
-                null
-                );
-           
-            try
-            {
-                sslStream.AuthenticateAsClient(serverName, GetX509CertificateCollection(), true);
-            }
-            catch (AuthenticationException e)
-            {
-                Console.WriteLine("Exception: {0}", e.Message);
-                if (e.InnerException != null)
-                {
-                    Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
-                }
-                Console.WriteLine("Authentication failed - closing the connection.");
-                client.Close();
-                return;
-            }
+		public static bool ValidateServerCertificate(object sender, X509Certificate certificate, X509Chain chain,
+			SslPolicyErrors sslPolicyErrors)
+		{
+			if (sslPolicyErrors == SslPolicyErrors.None)
+				return true;
 
-            string msg = GetHash(SHA256.Create(), "Hello from the client.");
-            byte[] messsage = Encoding.UTF8.GetBytes($"{msg}");
-         
-            sslStream.Write(messsage);
-            sslStream.Flush();
-            
-            string serverMessage = ReadMessage(sslStream);
-            Console.WriteLine("Server says: {0}", serverMessage);
-         
-            client.Close();
-            Console.WriteLine("Client closed.");
-        }
-        static string ReadMessage(SslStream sslStream)
-        {
-       
-            byte[] buffer = new byte[2048];
-            StringBuilder messageData = new StringBuilder();
-            int bytes = -1;
-            do
-            {
-                bytes = sslStream.Read(buffer, 0, buffer.Length);
+			Console.WriteLine("Certificate error: {0}", sslPolicyErrors);
 
-             
-                Decoder decoder = Encoding.UTF8.GetDecoder();
-                char[] chars = new char[decoder.GetCharCount(buffer, 0, bytes)];
-                decoder.GetChars(buffer, 0, bytes, chars, 0);
-                messageData.Append(chars);
-               
-                if (messageData.ToString().IndexOf("<EOF>") != -1)
-                {
-                    break;
-                }
-            } while (bytes != 0);
 
-            return messageData.ToString();
-        }
-        private static void DisplayUsage()
-        {
-            Console.WriteLine("To start the client specify:");
-            Console.WriteLine("clientSync machineName [serverName]");
-            Environment.Exit(1);
-        }
+			return false;
+		}
 
-        private static string GetHash(HashAlgorithm hashAlgorithm, string input)
-        {
+		public static void RunClient(string machineName, string serverName)
+		{
+			TcpClient client = new TcpClient(machineName, 8080);
+			Console.WriteLine("Client connected.");
 
-	        byte[] data = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+			SslStream sslStream = new SslStream(
+				client.GetStream(),
+				false,
+				new RemoteCertificateValidationCallback(ValidateServerCertificate),
+				null
+			);
 
-	        var sBuilder = new StringBuilder();
+			try
+			{
+				sslStream.AuthenticateAsClient(serverName, GetX509CertificateCollection(), true);
+			}
+			catch (AuthenticationException e)
+			{
+				Console.WriteLine("Exception: {0}", e.Message);
+				if (e.InnerException != null)
+				{
+					Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
+				}
 
-	        for (int i = 0; i < data.Length; i++)
-	        {
-		        sBuilder.Append(data[i].ToString("x2"));
-	        }
+				Console.WriteLine("Authentication failed - closing the connection.");
+				client.Close();
+				return;
+			}
 
-	        return sBuilder.ToString();
-        }
+			string plainMessage = "Hello from the client.";
+			
+			string hashedMessage = GetHash(SHA256.Create(), plainMessage);
+			byte[] messsage = Encoding.UTF8.GetBytes($"{plainMessage}-{hashedMessage}<EOF>");
 
-        public static X509CertificateCollection GetX509CertificateCollection()
-        {
-            string certificate = Directory.GetCurrentDirectory() + "\\client.pfx";
-            X509Certificate certificate1 = new X509Certificate(certificate, "secret");
-            X509CertificateCollection collection1 = new X509CertificateCollection
-            {
-                certificate1
-            };
-            return collection1;
-        }
- 
-    }
+			sslStream.Write(messsage);
+			sslStream.Flush();
+
+			string serverMessage = ReadMessage(sslStream);
+			Console.WriteLine("Server says: {0}", serverMessage);
+
+			client.Close();
+			Console.WriteLine("Client closed.");
+		}
+
+		static string ReadMessage(SslStream sslStream)
+		{
+			byte[] buffer = new byte[2048];
+			StringBuilder messageData = new StringBuilder();
+			int bytes = -1;
+			do
+			{
+				bytes = sslStream.Read(buffer, 0, buffer.Length);
+
+				Decoder decoder = Encoding.UTF8.GetDecoder();
+				char[] chars = new char[decoder.GetCharCount(buffer, 0, bytes)];
+				decoder.GetChars(buffer, 0, bytes, chars, 0);
+				messageData.Append(chars);
+
+				if (messageData.ToString().IndexOf("<EOF>") != -1)
+				{
+					break;
+				}
+			} while (bytes != 0);
+
+			return messageData.ToString();
+		}
+
+		private static string GetHash(HashAlgorithm hashAlgorithm, string input)
+		{
+			byte[] data = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+			var sBuilder = new StringBuilder();
+
+			for (int i = 0; i < data.Length; i++)
+			{
+				sBuilder.Append(data[i].ToString("x2"));
+			}
+
+			return sBuilder.ToString();
+		}
+
+		public static X509CertificateCollection GetX509CertificateCollection()
+		{
+			string certificate = Directory.GetCurrentDirectory() + "\\client.pfx";
+			X509Certificate certificate1 = new X509Certificate(certificate, "secret");
+			X509CertificateCollection collection1 = new X509CertificateCollection
+			{
+				certificate1
+			};
+			return collection1;
+		}
+	}
 }
