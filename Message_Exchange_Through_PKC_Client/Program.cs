a502b7ce4bb63fc7654e8ff5e24197c23463d7e1
Ruben van Oers <r.vanoers1@student.avans.nl>
2020-06-04 12:30:58 +0200

    added client authentication

--- Message_Exchange_Through_PKC_Client/Program.cs
+++ Message_Exchange_Through_PKC_Client/Program.cs
@@ -1,12 +1,127 @@
 ﻿using System;
+using System.Collections;
+using System.Net;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Authentication;
+using System.Text;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
 
-namespace Message_Exchange_Through_PKC_Client
+namespace Examples.System.Net
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Client.RunClient("HP-Envy-Ruben","127.0.0.1");
-		}
-	}
+    public class Program
+    {
+        private static Hashtable certificateErrors = new Hashtable();
+
+        // The following method is invoked by the RemoteCertificateValidationDelegate.
+        public static bool ValidateServerCertificate(
+              object sender,
+              X509Certificate certificate,
+              X509Chain chain,
+              SslPolicyErrors sslPolicyErrors)
+        {
+            if (sslPolicyErrors == SslPolicyErrors.None)
+                return true;
+
+            Console.WriteLine("Certificate error: {0}", sslPolicyErrors);
+
+            // Do not allow this client to communicate with unauthenticated servers.
+            return false;
+        }
+        public static void RunClient(string machineName, string serverName)
+        {
+	        // Create a TCP/IP client socket.
+            // machineName is the host running the server application.
+            TcpClient client = new TcpClient(machineName, 8080);
+            Console.WriteLine("Client connected.");
+            // Create an SSL stream that will close the client's stream.
+            SslStream sslStream = new SslStream(
+                client.GetStream(),
+                false,
+                new RemoteCertificateValidationCallback(ValidateServerCertificate),
+                null
+                );
+            // The server name must match the name on the server certificate.
+            try
+            {
+                sslStream.AuthenticateAsClient(serverName,GetX509CertificateCollection(),true);
+            }
+            catch (AuthenticationException e)
+            {
+                Console.WriteLine("Exception: {0}", e.Message);
+                if (e.InnerException != null)
+                {
+                    Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
+                }
+                Console.WriteLine("Authentication failed - closing the connection.");
+                client.Close();
+                return;
+            }
+            // Encode a test message into a byte array.
+            // Signal the end of the message using the "<EOF>".
+            byte[] messsage = Encoding.UTF8.GetBytes("Hello from the client.<EOF>");
+            // Send hello message to the server.
+            sslStream.Write(messsage);
+            sslStream.Flush();
+            // Read message from the server.
+            string serverMessage = ReadMessage(sslStream);
+            Console.WriteLine("Server says: {0}", serverMessage);
+            // Close the client connection.
+            client.Close();
+            Console.WriteLine("Client closed.");
+        }
+        static string ReadMessage(SslStream sslStream)
+        {
+            // Read the  message sent by the server.
+            // The end of the message is signaled using the
+            // "<EOF>" marker.
+            byte[] buffer = new byte[2048];
+            StringBuilder messageData = new StringBuilder();
+            int bytes = -1;
+            do
+            {
+                bytes = sslStream.Read(buffer, 0, buffer.Length);
+
+                // Use Decoder class to convert from bytes to UTF8
+                // in case a character spans two buffers.
+                Decoder decoder = Encoding.UTF8.GetDecoder();
+                char[] chars = new char[decoder.GetCharCount(buffer, 0, bytes)];
+                decoder.GetChars(buffer, 0, bytes, chars, 0);
+                messageData.Append(chars);
+                // Check for EOF.
+                if (messageData.ToString().IndexOf("<EOF>") != -1)
+                {
+                    break;
+                }
+            } while (bytes != 0);
+
+            return messageData.ToString();
+        }
+        private static void DisplayUsage()
+        {
+            Console.WriteLine("To start the client specify:");
+            Console.WriteLine("clientSync machineName [serverName]");
+            Environment.Exit(1);
+        }
+
+        public static X509CertificateCollection GetX509CertificateCollection()
+        {
+	        string certificate = Directory.GetCurrentDirectory() + "\\client.pfx";
+            X509Certificate certificate1 = new X509Certificate(certificate,"secret");
+            X509CertificateCollection collection1 = new X509CertificateCollection
+            {
+                certificate1
+            };
+            return collection1;
+        }
+        public static int Main(string[] args)
+        {
+            string serverCertificateName = "HP-Envy-Ruben";
+            string machineName = "HP-Envy-Ruben";
+           
+            Program.RunClient(machineName, serverCertificateName);
+            return 0;
+        }
+    }
 }
