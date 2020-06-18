09574182e133bb209263068517b28e568d391e6b
Ruben van Oers <r.vanoers1@student.avans.nl>
2020-06-18 19:40:44 +0200

    Assymmetrical encryption and symmetrical encryption working

--- Message_Exchange_Through_PKC_Sequel_Client/TcpSenderClient.cs
+++ Message_Exchange_Through_PKC_Sequel_Client/TcpSenderClient.cs
@@ -30,7 +30,7 @@ namespace Message_Exchange_Through_PKC_Sequel_Client
 					string message = Console.ReadLine();
 
 					//Hashen en signen
-					byte[] dataToHashAndEncrypt = Convert.FromBase64String(message);
+					byte[] dataToHashAndEncrypt = Encoding.UTF8.GetBytes(message);
 					byte[] encryptedHashMessage = SignMessage(dataToHashAndEncrypt,certificateClient);
 
 					var encryptionToSend = Convert.ToBase64String(encryptedHashMessage);
@@ -41,9 +41,33 @@ namespace Message_Exchange_Through_PKC_Sequel_Client
 						{ "hash", encryptionToSend }
 					};
 
-					byte[] jsonBytes = Encoding.ASCII.GetBytes(jsonObject.ToString());
+					byte[] encryptedData;
+					byte[] encryptedKey;
+					
+					using (Aes myAes = Aes.Create())
+					{
+						// Encrypt the string to an array of bytes.
+						encryptedData = EncryptStringToBytes_Aes(jsonObject.ToString(), myAes.Key, myAes.IV);
 
-					stream.Write(jsonBytes);
+						JObject symmetricalKey = new JObject
+						{
+							{"myAesKey", Convert.ToBase64String(myAes.Key)},
+							{"myAesIV", Convert.ToBase64String(myAes.IV)}
+						};
+
+						byte[] symmetricalKeyByte = Encoding.UTF8.GetBytes(symmetricalKey.ToString());
+
+						encryptedKey = Encrypt(symmetricalKeyByte, certificateServer);
+
+					}
+
+					JObject data = new JObject
+					{
+						{"encryptedData", encryptedData},
+						{"encryptedKey", encryptedKey}
+					};
+
+					stream.Write(Encoding.UTF8.GetBytes(data.ToString()));
 				}
 			}
 			catch (SocketException e)
@@ -52,10 +76,56 @@ namespace Message_Exchange_Through_PKC_Sequel_Client
 			}
 		}
 
+		private static byte[] Encrypt(byte[] symmetricalKey,X509Certificate2 certificate)
+		{
+			RSA publickey = certificate.GetRSAPublicKey();
+			return publickey.Encrypt(symmetricalKey, RSAEncryptionPadding.Pkcs1);
+		}
+
 		private static byte[] SignMessage(byte[] plainMessage, X509Certificate2 certificate)
 		{
 			RSA privateKey = certificate.GetRSAPrivateKey();
 			return privateKey.SignData(plainMessage, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+		}
+
+		static byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
+		{
+			// Check arguments.
+			if (plainText == null || plainText.Length <= 0)
+				throw new ArgumentNullException("plainText");
+			if (Key == null || Key.Length <= 0)
+				throw new ArgumentNullException("Key");
+			if (IV == null || IV.Length <= 0)
+				throw new ArgumentNullException("IV");
+			byte[] encrypted;
+
+			// Create an Aes object
+			// with the specified key and IV.
+			using (Aes aesAlg = Aes.Create())
+			{
+				aesAlg.Key = Key;
+				aesAlg.IV = IV;
+
+				// Create an encryptor to perform the stream transform.
+				ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+				// Create the streams used for encryption.
+				using (MemoryStream msEncrypt = new MemoryStream())
+				{
+					using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+					{
+						using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+						{
+							//Write all data to the stream.
+							swEncrypt.Write(plainText);
+						}
+						encrypted = msEncrypt.ToArray();
+					}
+				}
+			}
+
+			// Return the encrypted bytes from the memory stream.
+			return encrypted;
 		}
 	}
 }
--- Message_Exchange_Through_PKC_Sequel_Server/TcpReceiver.cs
+++ Message_Exchange_Through_PKC_Sequel_Server/TcpReceiver.cs
@@ -35,15 +35,31 @@ namespace Message_Exchange_Through_PKC_Sequel_Server
 						while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
 						{
 							// Translate data bytes to a ASCII string.
-							string jsonString = Encoding.ASCII.GetString(bytes);
+							string jsonString = Encoding.UTF8.GetString(bytes);
 
 							JObject json = JObject.Parse(jsonString);
 
-							string message = json.GetValue("message").ToString();
-							string signedData = json.GetValue("hash").ToString();
+							byte[] encryptedData = (byte[]) json.GetValue("encryptedData");
+							byte[] encryptedKey = (byte[]) json.GetValue("encryptedKey");
+
+							string encryptedKeyString =
+								Encoding.UTF8.GetString(Decrypt(encryptedKey, certificateServer));
+
+							JObject encryptedKeyJson = JObject.Parse(encryptedKeyString);
+
+
+							string message1 = DecryptStringFromBytes_Aes(encryptedData, Convert.FromBase64String(encryptedKeyJson.GetValue("myAesKey").ToString()),
+								Convert.FromBase64String(encryptedKeyJson.GetValue("myAesIV").ToString()));
+
+
+							JObject json1 = JObject.Parse(message1);
+
+							string message = json1.GetValue("message").ToString();
+							string signedData = json1.GetValue("hash").ToString();
+
 
 							SHA256Managed sha256Managed = new SHA256Managed();
-							var hashOfPlainMessage = sha256Managed.ComputeHash(Convert.FromBase64String(message));
+							var hashOfPlainMessage = sha256Managed.ComputeHash(Encoding.UTF8.GetBytes(message));
 
 							var x = Verify(hashOfPlainMessage, Convert.FromBase64String(signedData), certificateClient);
 
@@ -64,13 +80,62 @@ namespace Message_Exchange_Through_PKC_Sequel_Server
 				Console.WriteLine("SocketException: {0}", e);
 			}
 		}
-	
+
+		private static byte[] Decrypt(byte[] encryptedkey, X509Certificate2 certificate)
+		{
+			RSA privateKey = certificate.GetRSAPrivateKey();
+
+			return privateKey.Decrypt(encryptedkey, RSAEncryptionPadding.Pkcs1);
+		}
 
 		private static bool Verify(byte[] hashOfPlainMessage, byte[] signedData, X509Certificate2 certificate)
 		{
 			RSA publicKey = certificate.GetRSAPublicKey();
 
-			return publicKey.VerifyHash(hashOfPlainMessage, signedData, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+			return publicKey.VerifyHash(hashOfPlainMessage, signedData, HashAlgorithmName.SHA256,
+				RSASignaturePadding.Pkcs1);
+		}
+
+		static string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
+		{
+			// Check arguments.
+			if (cipherText == null || cipherText.Length <= 0)
+				throw new ArgumentNullException("cipherText");
+			if (Key == null || Key.Length <= 0)
+				throw new ArgumentNullException("Key");
+			if (IV == null || IV.Length <= 0)
+				throw new ArgumentNullException("IV");
+
+			// Declare the string used to hold
+			// the decrypted text.
+			string plaintext = null;
+
+			// Create an Aes object
+			// with the specified key and IV.
+			using (Aes aesAlg = Aes.Create())
+			{
+				aesAlg.Key = Key;
+				aesAlg.IV = IV;
+
+				// Create a decryptor to perform the stream transform.
+				ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+				// Create the streams used for decryption.
+				using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+				{
+					using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+					{
+						using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+						{
+							// Read the decrypted bytes from the decrypting stream
+							// and place them in a string.
+							plaintext = srDecrypt.ReadToEnd();
+						}
+					}
+				}
+			}
+
+			return plaintext;
 		}
 	}
 }
